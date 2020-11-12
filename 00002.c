d5d4c510dedfac9cd70d7db5048869c01fffceab
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-11-12 19:46:20 +0800

    Create 00002.c

---
+++ 00002.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void a()
+{
+    printf("lutingting\n");}
+void b()
+{
+    printf("nishiyizhizhu\n");
+}
+void c()
+{
+    printf("wohuiyizhixihuanniya\n");
+}
+void main()
+{
+    a();
+    b();
+    c();
+}
