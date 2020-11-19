ad757e5f30b80ccf7cf2b727e5ecc48dec1ad399
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-11-19 20:58:14 +0800

    Create 111920.图形.c

---
+++ 111920.图形.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void a()
+{printf("     ★\n");}
+void b()
+{printf("    ★★\n");}
+void c()
+{printf("   ★★★\n");}
+void d()
+{printf("  ★★★★\n");}
+void e()
+{printf(" ★★★★★ \n");}
+void main()
+{
+    a();
+    b();
+    c();
+    d();
+    e();
+}
