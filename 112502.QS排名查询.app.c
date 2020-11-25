4db2f32fbc79950a13d2f3a9433df59e9185aa87
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-11-25 09:53:04 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void main()
+{
+    int x;
+    printf("QS大学排名查询:");
+    scanf("%d",&x);
+    if(x==1)
+    {
+        printf("Massachusetts Institute of Technology");
+    }
+    else if(x==2)
+    {
+        printf("Stanford University");
+    }
+    else if(x==3)
+    {
+        printf("Harvard University");
+    }
+    else if(x==4)
+    {
+        printf("University of Oxford");
+    }
+    else if(x==5)
+    {
+        printf("加州理工学院");
+    }
+    else if(x==6)
+    {
+        printf("苏黎世联邦理工大学");
+    }
+    else if(x==7)
+    {
+        printf("剑桥大学");
+    }
+    else if(x==8)
+    {
+        printf("伦敦大学学院");
+    }
+    else if(x==9)
+    {
+        printf("帝国理工学院");
+    }
+    else if(x==10)
+    {
+        printf("芝加哥大学");
+    }
+    else
+    {
+        printf("加载中...");
+    }
+    system("color F1");
+}
