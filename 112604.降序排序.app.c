b841d335042438e0821396cee5e9f4da29b87ec1
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-11-26 23:30:51 +0800

    Create 112604.降序排序.app.c

---
+++ 112604.降序排序.app.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,t;
+    printf("请输入三个整数,请用逗号隔开:");
+    scanf("%d,%d,%d",&a,&b,&c);
+    if(a<b){
+        t=a;
+        a=b;
+        b=t;}
+    if(a<c){
+        t=a;
+        a=c;
+        c=t;}
+    if(b<c){
+        t=b;
+        b=c;
+        c=t;}
+    printf("这三个数从大到小排序是:\n");
+    printf("%d\n%d\n%d",a,b,c);
+}
