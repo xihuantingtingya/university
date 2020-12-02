6e21718f7b93173bb77a37f0df68f4d137ea631f
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-12-02 16:55:32 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -4,15 +4,17 @@ void main()
     int x,a,b,c;
     printf("请输入一个秒钟数:");
     scanf("%d",&x);
+    system("color F1");
     if(x>=0&&x<86400)
     {
         a=x/3600;       // 求小时
         b=(x%3600)/60;  //求分钟
         c=x-3600*a-60*b;//求秒
-        printf("%02d时：%02d分：%02d秒",a,b,c);
+        printf("%02d时：%02d分：%02d秒\n",a,b,c);
     }
     else
     {
         printf("报错!");
     }
+    system("pause");
 }
