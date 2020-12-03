301c21cd304c115ab7aa674f5200905b1208c507
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-12-03 21:54:42 +0800

    Update 120303.数组清零.app.c

--- 120303.数组清零.app.c
+++ 120303.数组清零.app.c
@@ -29,17 +29,21 @@ void main()
             for(j=0;j<10;j++)
                 {
             printf("%d ",arr[i][j]);
+            system("color F1");
                 }
             printf("\n");
         }
+        printf("请稍等五秒:\n");
         Sleep(5000);
         for(i=0;i<20;i++)
         {
             for(j=0;j<10;j++)
                 {
-            arr[i][j]=0;
-            printf("%d ",arr[i][j]);
+            arr[i][j]=000;
+            printf("%03d ",arr[i][j]);
+            system("color F4");
                 }
             printf("\n");
         }
 }
+
