7da0f0fcb1fdf4c320aad3f837075bed9037dfd3
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-12-09 16:25:41 +0800

    Create 120903.字符串的输出.app.c

---
+++ 120903.字符串的输出.app.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void a()
+{
+    char n[]="卢婷婷就是一个憨憨!";
+    printf("%s\n",n);
+}
+void b()
+{
+    char m[]="But I do like her.";
+    printf("%s\n",m);
+}
+void main()
+{
+    a();
+    b();
+    system("color F1");
+}
