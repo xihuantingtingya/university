25551efd6688fd91c8ccd85fed48ffb84a855dfc
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-12-11 01:39:32 +0800

    Update 121001.猜数字.app.c

--- 121001.猜数字.app.c
+++ 121001.猜数字.app.c
@@ -5,7 +5,7 @@ void main()
     printf("猜数字小游戏\n");
     printf("我们帅帅的C语言老师姓什么?\n");
     printf("您的答案:");
-    int x,y=12;
+    int x,y=12,i=5;
     char m[]="";
     char n="曹";
     scanf("%s",&m);
@@ -15,34 +15,51 @@ void main()
         printf("请输入:");
         while(x)
         {
+        i--;
         scanf("%d",&x);
         system("color F1");
+        if(i==0)
+        {
+            printf("您的机会用光啦\n");
+            printf("游戏结束了哦，憨憨");
+            break;
+        }
         if(x>20)
             {
                 printf("说好了1到20以内，你咋不遵守规则呢？\n");
                 printf("这下好了吧，游戏结束了");
                 break;
             }
-            if(x>y)
+        else if(x>y&&x<20)
             {
                 printf("您输入的值太大啦\n");
+                printf("您还有%d次机会\n",i);
                 printf("请您重新输入:");
+                if(i=0)
+        {
+            printf("您的机会用光啦\n");
+            printf("游戏结束了哦，憨憨");
+            break;
+        }
             }
-            if(x<y)
+        else if(x<y)
             {
                 printf("您输入的值太小啦\n");
+                printf("您还有%d次机会\n",i);
                 printf("请您重新输入:");
             }
-            if(x==y)
+        else
             {
                 printf("恭喜你猜对啦\n");
                 printf("这个数字是12");
                 break;
             }
         }
+
     }
     else
     {
-        printf("这么帅的老师都不知道，还想玩游戏，想屁吃");
+        printf("这么帅的老师姓什么都不知道，还想玩游戏，想屁吃");
     }
 }
+
