d18d4240c077f49e5481b3a04daa7fa180c4e40d
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2020-12-16 16:36:20 +0800

    Update 121601.递归求和.app.c

--- 121601.递归求和.app.c
+++ 121601.递归求和.app.c
@@ -13,18 +13,26 @@ void main()
     printf("请输入您想的数字：");
     while(1)
     {
-    j--;
-    if(j>=0)
-    {
         scanf("%d",&i);
-        printf("递归后：%d\n",myFunaction(i));
-        printf("您还有%d次操作机会\n",j);
-        printf("请继续：");
-    }
-    else
-    {
-        printf("您的操作机会用光啦，请充值！");
-        break;
-    }
+        j--;
+        if(j<0)
+        {
+            printf("您的机会用完了哦!");
+        }
+        else
+        {
+            if(j>0)
+            {
+                printf("递归后：%d\n",myFunaction(i));
+                printf("您还有%d次操作机会\n",j);
+                printf("请继续：");
+            }
+            else
+            {
+                printf("递归后：%d\n",myFunaction(i));
+                printf("您的操作机会用光啦，请充值！");
+                break;
+            }
+        }
     }
 }
