ae8847074bf87ac940485a6b817859b3fd84c2c1
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2021-01-28 22:45:32 +0800

    Update 112504.秒表计数器.app.c

--- 112504.秒表计数器.app.c
+++ 112504.秒表计数器.app.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+#include<windows.h>
 void main()
 {
     int i=0;
     printf("欢迎使用秒表计时器:\n");
-    for(i=i+1;i<=10^10;i++)
-        printf("%d秒过去了\n",i);
+    while(1)
+    {
+        i++;
+        system("cls");
+        printf("第%d秒过去了\n",i);
+        Sleep(1000);
+    }
 }
